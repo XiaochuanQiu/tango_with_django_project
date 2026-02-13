22d4514f97792b201401e3e57ca724755c4d6cfa
Xiaochuan Qiu <2903477Q@student.gla.ac.uk>
2026-02-13 13:45:13 +0000

    chapter 7.2

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 # Create your views here.
 def index(request):
     
     category_list = Category.objects.order_by('-likes')[:5]
-    
     page_list = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
@@ -14,6 +14,9 @@ def index(request):
     context_dict['pages'] = page_list
     
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
     
@@ -34,3 +37,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
         
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+            
+    return render(request, 'rango/add_category.html', {'form': form})
